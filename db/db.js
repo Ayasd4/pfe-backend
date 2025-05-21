bc60f9cdb9b16579767afa9a017da978b9820d66
Ayasd4 <sdiriaya488@gmail.com>
2025-05-21 13:05:12 +0100

    update

--- db/db.js
+++ db/db.js
@@ -1,3 +1,5 @@
+
+
 const { Pool } = require('pg'); // Importer Pool depuis pg
 
 // Créer une instance de Pool avec tes paramètres de connexion
@@ -5,24 +7,24 @@ const { Pool } = require('pg'); // Importer Pool depuis pg
 // Créer une instance de Pool avec tes paramètres de connexion
 
 // base de données local
-const pool = new Pool({
+/*const pool = new Pool({
     host: "localhost",
     user: "postgres",
     port: 5432,
     password: "0000",        
     database: "flotte" 
-});
+});*/
 
 
 
 //base de données en ligne
 
-/*const pool = new Pool({
-  connectionString: 'postgresql://neondb_owner:npg_w7lbz2kCsouh@ep-green-sun-a45pxs2v-pooler.us-east-1.aws.neon.tech/gestion%20de%20flotte?sslmode=require',
+const pool = new Pool({
+  connectionString: 'postgresql://neondb_owner:npg_BXTIizD4wvZ9@ep-summer-wave-a50xyz89-pooler.us-east-2.aws.neon.tech/neondb?sslmode=require',
   ssl: {
     rejectUnauthorized: false,
   },
-});*/
+});
 
 
 // Tester la connexion
